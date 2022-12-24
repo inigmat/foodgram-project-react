35ca7bbc7eab131827d9ca7544b8dcf4b0a60c9e
Ильгиз Нигматуллин <iznigmatullin@ya.ru>
2022-12-24 17:10:18 +0300

    IsAuthorOrAdminOrReadOnly permission upd

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,12 +2,21 @@ from rest_framework import permissions
 
 
 class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, _view):
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_authenticated)
+    
     def has_object_permission(self, request, _view, obj):
-        if request.user.is_authenticated and (
-                request.user.is_admin or obj.author
-                == request.user or request.method == 'POST'):
-            return True
-        return request.method in permissions.SAFE_METHODS
+        return (request.method in permissions.SAFE_METHODS
+                or request.user.is_admin
+                or obj.author == request.user)
+
+#    def has_object_permission(self, request, _view, obj):
+#        if request.user.is_authenticated and (
+#                request.user.is_admin or obj.author
+#                == request.user or request.method == 'POST'):
+#            return True
+#        return request.method in permissions.SAFE_METHODS
 
 
 class IsAdminOrReadOnly(permissions.BasePermission):
