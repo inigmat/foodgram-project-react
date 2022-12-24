3b7c43fef4427a30c2b01060e286d3f5a2f96bd6
Ильгиз Нигматуллин <iznigmatullin@ya.ru>
2022-12-24 16:52:46 +0300

    IsAdminOrReadOnly permission upd

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -13,7 +13,7 @@ class IsAuthorOrAdminOrReadOnly(permissions.BasePermission):
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, _view):
         return (
-            request.method in ('GET',)
+            request.method in permissions.SAFE_METHODS
             or request.user.is_authenticated
             and request.user.is_admin
         )
